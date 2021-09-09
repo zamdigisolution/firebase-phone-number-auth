8d75cffdfe42b8adeaf971cf4ae604c54176bc42
zamdigisolution <zamdigisolution@gmail.com>
2021-09-09 22:56:12 +0530

    fix

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-var recaptcha = new firebase.auth.RecaptchaVerifier('recaptcha');
+//var recaptcha = new firebase.auth.RecaptchaVerifier('recaptcha');
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBMQWhLZqivwYIhLldujsUurlM-p9mUxRU",
+  authDomain: "ll-cust-subprod.firebaseapp.com",
+  databaseURL: "https://ll-cust-subprod.firebaseio.com",
+  projectId: "ll-cust-subprod",
+  storageBucket: "ll-cust-subprod.appspot.com",
+  messagingSenderId: "956727394482",
+  appId: "1:956727394482:web:a8109242d3a90f704409ab",
+  measurementId: "G-15QL6X9PPQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const recaptcha = app.auth.RecaptchaVerifier('recaptcha');
+
 
 function auth(){
 
     var number = '+91' + document.querySelector('input').value;
   
-    firebase.auth().signInWithPhoneNumber(number, recaptcha).then( function(e) {
+    app.auth().signInWithPhoneNumber(number, recaptcha).then( function(e) {
         console.log('successful text message');
 
 
