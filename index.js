af21f649f7f6323737a7d49e7cb693d05322557d
zamdigisolution <zamdigisolution@gmail.com>
2021-09-09 22:38:33 +0530

    updated

--- index.js
+++ index.js
@@ -2,32 +2,32 @@ var recaptcha = new firebase.auth.RecaptchaVerifier('recaptcha');
 
 function auth(){
 
-    var number = '+82' + document.querySelector('input').value;
+    var number = '+91' + document.querySelector('input').value;
   
     firebase.auth().signInWithPhoneNumber(number, recaptcha).then( function(e) {
-        console.log('문자 전송 성공');
+        console.log('successful text message');
 
 
         var 
-            code = prompt('인증 번호를 입력해주세요', '');
+            code = prompt('Please enter your verification number ', '');
 
         
         if(code === null) return;
 
         
         e.confirm(code).then(function (result) {
-            console.log('인증 성공', result.user);
+            console.log('authentication success', result.user);
 
-            document.querySelector('label').textContent += '성공 ' + result.user.phoneNumber;
+            document.querySelector('label').textContent += 'Success ' + result.user.phoneNumber;
             
         }).catch(function (error) {
-            console.error('인증 실패', error);
+            console.error('authentication failed', error);
             
         });
 
     })
     .catch(function (error) {
-        console.error('문자 전송 실패', error);
+        console.error('Failed to send text', error);
 
     });
   
